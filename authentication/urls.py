88ae65379be85f4a378995c9c281f21bb1c002d8
Michael Awortwe <michael.awortwe@yahoo.com>
2023-09-10 21:25:29 +0000

    EXPENSES-19 Set password

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserLogin,UserSignUp
+from .views import UserLogin,UserSignUp, ResetPassword
 
 urlpatterns = [
     path('',UserLogin.as_view(), name ="login"),
     path('signup',UserSignUp.as_view(), name ="signup"),
+    path('reset',ResetPassword.as_view(), name ="reset"),
 ]
